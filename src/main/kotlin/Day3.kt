b5265276471d99b2d09d4e40b0a4c12856dd4101
Sebs <sebastian@helzer.eu>
2021-12-03 23:18:07 -0600

    Day 3 Part 2
    Life Support Rating

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -8,14 +8,44 @@ fun processPowerConsumption(filename: String): Int {
 }
 
 fun getLifeSupportRating(filename: String): Int {
-    return 0
+    val binaryNumbers: List<String> = readBinaryNumbersFromFile(filename)
+    val allTheBits = intsToBits(binaryNumbers.map { it.toInt(2) })
+    val oxyBits = filterNumberByBit(allTheBits)
+    val oxygen = bitsToInt(oxyBits)
+
+    val co2Bits = filterNumberByBitBackward(allTheBits)
+    val cO2 = bitsToInt(co2Bits)
+
+    return oxygen * cO2
+}
+
+private fun filterNumberByBit(bits: List<List<Boolean>>, bitIndex: Int = bits.maxOf { it.size - 1 }): List<Boolean> {
+    if(bits.size == 1) return bits.first()
+    if(bitIndex < 0) throw Error("We were not able to filter well")
+    val bitCount = bits.count { it.getOrNull(bitIndex) ?: false }
+    return if (bitCount >= bits.size - bitCount) {
+        filterNumberByBit(bits.filter { it.getOrNull(bitIndex) ?: false }, bitIndex - 1)
+    } else {
+        filterNumberByBit(bits.filter { !(it.getOrNull(bitIndex) ?: false) }, bitIndex - 1)
+    }
+}
+
+private fun filterNumberByBitBackward(bits: List<List<Boolean>>, bitIndex: Int = bits.maxOf { it.size - 1}): List<Boolean> {
+    if(bits.size == 1) return bits.first()
+    if(bitIndex < 0) throw Error("We were not able to filter well")
+    val bitCount = bits.count { it.getOrNull(bitIndex) ?: false }
+    return if (bitCount >= bits.size - bitCount) {
+        filterNumberByBitBackward(bits.filter { !(it.getOrNull(bitIndex) ?: false) }, bitIndex - 1)
+    } else {
+        filterNumberByBitBackward(bits.filter { (it.getOrNull(bitIndex) ?: false) }, bitIndex - 1)
+    }
 }
 
 private fun readBinaryNumbersFromFile(filename: String): List<String> = File(filename).readLines()
 
 private fun getGammaAndEpsilon(numbers: List<String>): Pair<Int, Int> {
     val bytes = numbers.map { it.toInt(2) }
-    val allTheBits = convertToBitsUnlimited(bytes)
+    val allTheBits = intsToBits(bytes)
 
     val bits = mashBitsIntoBits(allTheBits, numbers.size/2)
     val gamma = getGamma(bits)
@@ -27,20 +57,21 @@ private fun getGammaAndEpsilon(numbers: List<String>): Pair<Int, Int> {
     return gamma to epsilon
 }
 
-private fun convertToBitsUnlimited(ints: List<Int>): List<List<Boolean>> {
-    return ints.map { num -> (0 until num.toString(2).length).map {
-            num.and(2.0.pow(it).toInt()) > 0
-        }
+private fun intsToBits(ints: List<Int>): List<List<Boolean>> = ints.map { num -> intToBits(num) }
+
+private fun intToBits(int: Int): List<Boolean> = (0 until int.toString(2).length).map {
+    int.and(2.0.pow(it).toInt()) > 0
+}
+
+private fun mashBitsIntoBits(bits: List<List<Boolean>>, halfSize: Int): List<Boolean> {
+    return (0 until bits.maxOf { it.size }).map { index ->
+        bits.count { it.getOrNull(index) ?: false } > halfSize
     }
 }
 
-private fun mashBitsIntoBits(allTheBits: List<List<Boolean>>, halfSize: Int): List<Boolean> {
-    return (0 until allTheBits.maxOf { it.size }).map { index ->
-        allTheBits.count { it.getOrNull(index) ?: false } > halfSize
-    }
-}
+private fun getGamma(bits: List<Boolean>): Int = bitsToInt(bits)
 
-private fun getGamma(bits: List<Boolean>): Int {
+private fun bitsToInt(bits: List<Boolean>): Int {
     return bits.mapIndexed { index, b -> if (b) 2.0.pow(index).toInt() else 0 }.sum()
 }
 
