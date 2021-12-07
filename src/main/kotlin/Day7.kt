35f44fb898390a9aaed9ba604aa3066b7cbec0d4
Sebs <sebastian@helzer.eu>
2021-12-07 08:41:44 -0600

    Clean Day 7
    Make it tidy

--- src/main/kotlin/Day7.kt
+++ src/main/kotlin/Day7.kt
@@ -4,19 +4,10 @@ import kotlin.math.abs
 fun findLowestCrabSubmarineFuelConsumption(filename: String, trueConsumption: Boolean = false): Int {
     val crabSubsPos = getNumbersFromFileWithDDelimiter(filename)
     return ((crabSubsPos.minOrNull() ?: 0)..(crabSubsPos.maxOrNull() ?: 0))
-        .minOfOrNull {
-            if(trueConsumption) {
-                calculateTrueFuelConsumption(crabSubsPos, it)
-            } else {
-                calculateFuelConsumption(crabSubsPos, it)
-            }
-        } ?: throw Error("Unable to find min position")
+        .minOfOrNull { calculateTrueFuelConsumption(crabSubsPos, it, trueConsumption) }
+        ?: throw Error("Unable to find min position")
 }
 
-private fun calculateFuelConsumption(subs: List<Int>, target: Int): Int {
-    return subs.sumOf { abs(it - target) }
-}
-
-private fun calculateTrueFuelConsumption(subs: List<Int>, target: Int): Int {
-    return subs.sumOf { (0 .. abs(it - target)).sum() }
+private fun calculateTrueFuelConsumption(subs: List<Int>, target: Int, trueConsumption: Boolean = false): Int {
+    return subs.sumOf { if (trueConsumption) (0 .. abs(it - target)).sum() else abs(it - target) }
 }
